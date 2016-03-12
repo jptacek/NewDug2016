d35d821b6f2729a317e24919fb3d082be79396a5
John Ptacek <jptacek@live.com>
2016-03-12 08:56:26 -0600

    Put bootstrap file in location

---
+++ gulpFileConfig.js
@@ -0,0 +1,39 @@
+module.exports = function() {
+
+    var distDirectory = './dist/';
+    var srcDirectory = './src/';
+    var bowerDirectory = srcDirectory + 'bower_components/';
+
+    var config = {
+        // create configuration
+
+        paths: {
+            distDirectory: distDirectory,
+            srcDirectory: srcDirectory,
+            html: srcDirectory + '/**/*.html',
+            images: './src/images/**/*',
+            js: [
+                bowerDirectory + '**/*.min.js',
+                srcDirectory + 'scripts/chemistryApp.js',
+                srcDirectory + 'scripts/chemistryController.js',
+                srcDirectory + 'scripts/chemistryDirective.js',
+                srcDirectory + 'scripts/chemistryService.js',
+            ],
+            css: [
+                bowerDirectory + 'bootstrap/dist/css/bootstrap.min.css',
+                srcDirectory + 'css/chemistry.css'
+            ],
+            jsValidate: [
+                srcDirectory + 'scripts/**/*.js'
+            ],
+            sass: srcDirectory + 'sass/*.scss',
+            json: srcDirectory +'scripts/*.json'
+        },
+                    port: 3000,
+            devBaseUrl: 'http://localhost'
+
+
+    };
+
+    return config;
+};
